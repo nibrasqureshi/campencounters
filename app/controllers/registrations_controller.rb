aabc7e158aa19f735f9b7dd213aba7a045d5d72f
nibrasqureshi <nibrasqureshi1108@gmail.com>
2022-09-07 15:18:47 +0500

    add validation for user model

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,7 +11,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:agreement, :first_name, :last_name, :email, :password, :phone, :country)
+      u.permit(:agreement, :first_name, :last_name, :email, :password, :password_confirmation, :phone, :country)
     end
 
     devise_parameter_sanitizer.permit(:account_update) do |u|
