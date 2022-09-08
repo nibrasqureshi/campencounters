eb1eaf2fb4b5fe45f9cb57ccd29fdf7a9b6d6a17
nibrasqureshi <nibrasqureshi1108@gmail.com>
2022-09-08 11:15:27 +0500

    User controller bugs fixed

--- Gemfile
+++ Gemfile
@@ -22,9 +22,9 @@ gem 'uglifier', '>= 1.3.0'
 # gem devise authentication
 gem 'bootstrap', '~> 5.2.0'
 gem 'bootstrap5-kaminari-views'
-gem 'kaminari'
 gem 'devise'
 gem 'jquery-rails'
+gem 'kaminari'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,49 +1,52 @@
 # frozen_string_literal:true
 
 # this is a user controller for admin
-class Admin::UsersController < ApplicationController
-  before_action :find_user, only: %i[show edit update destroy]
-  def index
-    @users = User.all.page(params[:page])
-  end
-
-  def show; end
-
-  def edit; end
-
-  def destroy
-    @user.destroy
-    redirect_to admin_users_path
-  end
-
-  def update
-    if @user.update(post_params)
-      redirect_to([:admin, @user])
-    else
-      render 'edit'
+module Admin
+  # this is a user controller class for admin
+  class UsersController < ApplicationController
+    before_action :find_user, only: %i[show edit update destroy]
+    def index
+      @users = User.all.page(params[:page])
     end
-  end
 
-  def new
-    @user = User.new
-  end
+    def show; end
 
-  def create
-    @user = User.new(post_params)
-    if @user.save
-      redirect_to @user
-    else
-      render 'new'
+    def edit; end
+
+    def destroy
+      @user.destroy
+      redirect_to admin_users_path
     end
-  end
 
-  private
+    def update
+      if @user.update(post_params)
+        redirect_to([:admin, @user])
+      else
+        render 'edit'
+      end
+    end
 
-  def find_user
-    @user = User.find(params[:id])
-  end
+    def new
+      @user = User.new
+    end
 
-  def post_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    def create
+      @user = User.new(post_params)
+      if @user.save
+        redirect_to @user
+      else
+        render 'new'
+      end
+    end
+
+    private
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+    def post_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
   end
 end
