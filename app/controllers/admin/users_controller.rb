1edfb95d8124ef848b7ace2a0ca87af33e27d9d1
nibrasqureshi <nibrasqureshi1108@gmail.com>
2022-09-08 13:10:17 +0500

    updated the admin listing

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,52 +1,58 @@
 # frozen_string_literal:true
 
-# this is a user controller for admin
-module Admin
-  # this is a user controller class for admin
-  class UsersController < ApplicationController
-    before_action :find_user, only: %i[show edit update destroy]
-    def index
-      @users = User.all.page(params[:page])
+# this is a user controller class for admin
+class Admin::UsersController < ApplicationController
+  before_action :find_user, only: %i[show edit update destroy]
+  helper_method :sort_column, :sort_direction
+  def index
+    @users = User.order("#{sort_column} #{sort_direction}").page(params[:page])
+  end
+
+  def show; end
+
+  def edit; end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+  def update
+    if @user.update(post_params)
+      redirect_to([:admin, @user])
+    else
+      render 'edit'
     end
+  end
 
-    def show; end
+  def new
+    @user = User.new
+  end
 
-    def edit; end
-
-    def destroy
-      @user.destroy
-      redirect_to admin_users_path
+  def create
+    @user = User.new(post_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
     end
+  end
 
-    def update
-      if @user.update(post_params)
-        redirect_to([:admin, @user])
-      else
-        render 'edit'
-      end
-    end
+  private
 
-    def new
-      @user = User.new
-    end
+  def find_user
+    @user = User.find(params[:id])
+  end
 
-    def create
-      @user = User.new(post_params)
-      if @user.save
-        redirect_to @user
-      else
-        render 'new'
-      end
-    end
+  def post_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 
-    private
+  def sort_column
+    params[:sort] || 'first_name'
+  end
 
-    def find_user
-      @user = User.find(params[:id])
-    end
-
-    def post_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password)
-    end
+  def sort_direction
+    params[:direction] || 'asc'
   end
 end
