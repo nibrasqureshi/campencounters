93c524b4de416f46e9b594392ea570a96706e83e
nibrasqureshi <nibrasqureshi1108@gmail.com>
2022-09-13 14:57:22 +0500

    updated suggested changes

--- app/helpers/camp_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal:true
-
-# this is application helper
-module CampHelper
-  def locations
-    JSON.parse(locations)
-  end
-end
--- app/helpers/user_helper.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal:true
-
-# this is application helper
-module UserHelper
-end
